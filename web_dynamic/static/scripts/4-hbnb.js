4d1bf31ca436126a6f835f58b5709aa3bc5cfbb0
jgnacio <jgnaciogomez406@gmail.com>
2023-04-26 11:21:39 -0700

    Add: function getPlaces and handle 2-hbnb.js

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,36 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-  let listAmenities = [];
-  let listIdofAmen = [];
-  $("input[type=checkbox]").prop("checked", false); // Joaco credits :)
-  $("input[type=checkbox]").change(function () {
-    if (this.checked) {
-      listAmenities.push($(this).attr("data-name"));
-      listIdofAmen.push($(this).attr("data-id"));
-    } else {
-      listAmenities.splice(listAmenities.indexOf($(this).attr("data-name")), 1);
-      listIdofAmen.splice(listIdofAmen.indexOf($(this).attr("data-id")), 1);
-    }
-    $(".amenities > h4").text(listAmenities.join(", "));
-  });
-
-  let body_data = '{}';
-  const btn = document.querySelector('button');
-    btn.addEventListener('click', function(event){
-    if (listIdofAmen.length > 0) {
-    body_data = {'amenities': listIdofAmen};
-    }
-  });
-
+function getPlaces(body_data) {
   fetch("http://0.0.0.0:5001/api/v1/places_search/", {
     method: "POST",
     headers: {
-      "Content-Type": "application/json",
+      "Content-Type": "application/json"
     },
-    body: body_data,
+    body: JSON.stringify(body_data)
   })
-    .then((response) => response.json())
-    .then((data) => {
+    .then(response => {
+      let div = document.querySelector("#api_status");
+      if (response.ok) {
+        div.classList.add("available");
+      } else {
+        div.classList.remove("available");
+      }
+
+      return response.json();
+    })
+    .then(data => {
       const sectionPlace = document.querySelector("section.places");
+      sectionPlace.innerHTML = "";
       for (let place of data) {
         const article = document.createElement("article");
         article.innerHTML = `
@@ -39,15 +27,17 @@ document.addEventListener("DOMContentLoaded", () => {
           <div class="price_by_night">${place.price_by_night}</div>
         </div>
         <div class="information">
-          <div class="max_guest">${place.max_guest} Guest${
-          place.max_guest != 1 ? "s" : ""
-        }</div>
-          <div class="number_rooms">${place.number_rooms} Bedroom${
-          place.number_rooms != 1 ? "s" : ""
-        }</div>
-          <div class="number_bathrooms">${place.number_bathrooms} Bathroom${
-          place.number_bathrooms != 1 ? "s" : ""
-        }</div>
+          <div class="max_guest">${place.max_guest} Guest${place.max_guest != 1
+          ? "s"
+          : ""}</div>
+          <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !=
+        1
+          ? "s"
+          : ""}</div>
+          <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !=
+        1
+          ? "s"
+          : ""}</div>
         </div>
         <div class="user"><b>Owner:</b> </div>
             <div class="description">
@@ -58,7 +48,33 @@ document.addEventListener("DOMContentLoaded", () => {
         sectionPlace.appendChild(article);
       }
     })
-    .catch(function (error) {
+    .catch(function(error) {
       console.log(error);
     });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  let listAmenities = [];
+  let listIdofAmen = [];
+  $("input[type=checkbox]").prop("checked", false); // Joaco credits :)
+  $("input[type=checkbox]").change(function() {
+    if (this.checked) {
+      listAmenities.push($(this).attr("data-name"));
+      listIdofAmen.push($(this).attr("data-id"));
+    } else {
+      listAmenities.splice(listAmenities.indexOf($(this).attr("data-name")), 1);
+      listIdofAmen.splice(listIdofAmen.indexOf($(this).attr("data-id")), 1);
+    }
+    $(".amenities > h4").text(listAmenities.join(", "));
+  });
+
+  let body_data = {};
+  const btn = document.querySelector("button");
+  btn.addEventListener("click", function(event) {
+    if (listIdofAmen.length > 0) {
+      body_data = { amenities: listIdofAmen };
+    }
+    getPlaces(body_data);
+  });
+  getPlaces(body_data);
 });
